55be3a5a87812c9ed65ca47818053cf31b4d740b
Rizky Hermawan <rhermawan56@gmail.com>
2022-10-06 23:03:23 +0700

    production-4: step-1 done

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,4 @@ Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::post('/{transaction}', [DashboardAdminController::class, 'updateDone'])->middleware('auth');
 
-Route::resource('/dashboard/transaction', DashboardAdminController::class)->middleware('role:Admin');
+Route::resource('/dashboard/transaction', DashboardAdminController::class)->middleware('auth');
